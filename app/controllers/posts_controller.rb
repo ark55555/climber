bcbd28cf78201d4de91fa7d7c38a7ce51447ef2e
ark55555 <ark.tooyoung@gmail.com>
2021-06-08 04:22:18 +0000

    [Add]post index

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,22 +1,50 @@
 class PostsController < ApplicationController
-  
+
+
   def new
     @post = Post.new
-    
   end
-  
+
   def create
+    @post = Post.new(post_params)
+    @post.user_id = current_user.id
+    if @post.save
+      redirect_to post_path(@post), notice: "投稿ありがとうございます"
+    else
+      render :new
+    end
   end
-  
+
   def index
+    @posts = Post.all.page(params[:page]).per(8)
+  end
+
+  def show
+    @post = Post.find(params[:id])
   end
 
   def edit
+    @post = Post.find(params[:id])
   end
-  
+
   def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to post_path(@post), notice: "投稿情報更新しました"
+    else
+      render "edit"
+    end
   end
-  
+
   def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path, notice: "投稿を削除しました"
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:goods_name, :image, :caption)
   end
 end
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'e432996ef0fbd9189c1e271d2632e7b617e92314b0ddff839964989f6b57011ff2b0b8db1055e8748e922f4aa60df838782af0db699999144072d67f75e542ab'
