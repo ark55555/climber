0747fe7c4a446f281890ce456f2eeab21ae65e1d
ark55555 <ark.tooyoung@gmail.com>
2021-06-12 06:54:43 +0000

    [Add]user/edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,36 +1,46 @@
 class UsersController < ApplicationController
   before_action :ensure_correct_user, only: [:edit, :update, :destroy]
-  
-  def show 
+
+  def show
     @user = User.find(params[:id])
     @posts = @user.posts.all.page(params[:page]).per(6)
   end
-  
+
   def bookmark
     bookmarks = Bookmark.where(user_id: current_user.id).pluck(:post_id)  # ログイン中のユーザーのお気に入りのpost_idカラムを取得
     @bookmark_list = Post.find(bookmarks)
   end
-  
+
   def edit
     @user = User.find(params[:id])
   end
 
   def update
+    if @user.update(user_params)
+      redirect_to user_path(@user), notice: "ユーザー情報を編集しました!"
+    else
+      render "edit"
+    end
   end
-  
+
   def confirm
   end
 
   def retire
   end
-  
+
   private
-  
+
+  def user_params
+    params.require(:user).permit(:name, :introduction, :profile_image)
+  end
+
   def ensure_correct_user
     @user = User.find(params[:id])
     unless @user == current_user
       redirect_to user_path(current_user)
+      flash[:warning] = '自分のユーザー情報以外は変更することができません'
     end
   end
-  
+
 end
