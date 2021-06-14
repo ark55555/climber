33811705c5e2b284a04f792e3647cfdc03a4c364
ark55555 <ark.tooyoung@gmail.com>
2021-06-14 00:19:07 +0000

    [Add]like/Ajax

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,17 +1,15 @@
 class LikesController < ApplicationController
-  
+
   def create
     @post = Post.find(params[:post_id])
     like = @post.likes.new(user_id: current_user.id)
     like.save
-    redirect_back(fallback_location: root_path)
   end
-  
+
   def destroy
     @post = Post.find(params[:post_id])
     like = @post.likes.find_by(user_id: current_user.id)
     like.destroy
-    redirect_back(fallback_location: root_path)
   end
 
 end
