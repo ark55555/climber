c823c4e28758f1d914d74dd0597c51c6f5cc4de0
ark55555 <ark.tooyoung@gmail.com>
2021-06-15 07:22:49 +0000

    [Add]admin

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_10_103345) do
+ActiveRecord::Schema.define(version: 2021_06_15_062620) do
 
   create_table "bookmarks", force: :cascade do |t|
     t.integer "user_id"
@@ -63,6 +63,7 @@ ActiveRecord::Schema.define(version: 2021_06_10_103345) do
     t.boolean "is_active", default: false, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.create!(
+  name: 'administrator',
+  email: 'admin@admin.com',
+  password: ENV['ADMIN_PASS'],
+  password_confirmation: ENV['ADMIN_PASS'],
+  profile_image: File.open('./app/assets/images/admin_img.png'),
+  admin: true
+)
