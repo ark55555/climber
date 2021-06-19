d1d0e90f84549b11493a5f568896c1914a084fdd
ark55555 <ark.tooyoung@gmail.com>
2021-06-19 07:43:20 +0000

    [Add]admin-post/comment

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -3,9 +3,8 @@ class PostCommentsController < ApplicationController
 
   def create
     @post = Post.find(params[:post_id])
-    @post_comment = PostComment.new(post_comment_params)
+    @post_comment = current_user.post_comments.new(post_comment_params)
     @post_comment.post_id = @post.id
-    @post_comment.user_id = current_user.id
     @post_comment.save
   end
 
@@ -13,13 +12,12 @@ class PostCommentsController < ApplicationController
     @post = Post.find(params[:post_id])
     post_comment = @post.post_comments.find(params[:id])
     post_comment.destroy
-
   end
 
   private
 
   def post_comment_params
-    params.require(:post_comment).permit(:comment, :post_id, :user_id)
+    params.require(:post_comment).permit(:comment)
   end
 
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create, :show, :edit, :update, :destroy, :search, :bookmarks]
+  before_action :authenticate_user!, except: [:index]
+  before_action :ensure_correct_user, only: [:edit, :update, :destroy]
 
   def new
     @post = Post.new
@@ -26,7 +27,7 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
     @user = @post.user
     @post_comment = PostComment.new
-    @post_comments = @post.post_comments.order("created_at DESC")
+    @post_comments = @post.post_comments
     @post_tags = @post.tags
   end
 
@@ -42,12 +43,10 @@ class PostsController < ApplicationController
   end
 
   def edit
-    @post = Post.find(params[:id])
     @tag_list = @post.tags.pluck(:tag_name).join(",")
   end
 
   def update
-    @post = Post.find(params[:id])
     tag_list = params[:post][:tag_name].split(",")
     if @post.update(post_params)
       @post.save_tag(tag_list)
@@ -58,7 +57,6 @@ class PostsController < ApplicationController
   end
 
   def destroy
-    @post = Post.find(params[:id])
     @post.destroy
     redirect_to posts_path, secondary: "投稿を削除しました"
   end
@@ -68,4 +66,12 @@ class PostsController < ApplicationController
   def post_params
     params.require(:post).permit(:goods_name, :image, :caption, :rate)
   end
+
+  def ensure_correct_user
+    @post = Post.find(params[:id])
+    unless @post.user == current_user || current_user.admin?
+      redirect_to posts_path, warning: "自分の投稿以外の情報は変更することができません"
+    end
+  end
+
 end
